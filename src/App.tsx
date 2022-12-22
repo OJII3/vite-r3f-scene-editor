03341dc226c2379c09823b14aca485d020224cf5
OJII3 <84656786+OJII3@users.noreply.github.com>
2022-12-22 09:40:03 +0900

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Sphere, Sparkles } from "@react-three/drei";
+
+const App = () => (
+	<Canvas camera={{ fov: 45, position: [-4, 2, -4] }} className='canvas'>
+		<Sparkles position={[1, 1, 1]} />
+		<OrbitControls makeDefault />
+		<mesh position={[6.948, -2.158, 0.465]}>
+			<boxBufferGeometry attach="geometry" />
+			<meshStandardMaterial attach="material" color={"rgb(254, 0, 255)"} wireframe={false} />
+		</mesh>
+		<directionalLight position={[4.224, 1.912, 3.046]} castShadow={true} />
+		<ambientLight intensity={1} />
+		<Sphere position={[8.817, 1.557, 5.818]} material-reflectivity={0.8} material-opacity={1} material-transparent={false} />
+	</Canvas>
+);
+
+export default App;
